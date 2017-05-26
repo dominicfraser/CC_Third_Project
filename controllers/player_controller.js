ba90c43ce9a3e42c3dba86f0617eaa6d4e574b84
Dominic <mrdjfraser@gmail.com>
2017-05-26 10:52:59 +0100

    bar and player controllers

---
+++ controllers/player_controller.js
@@ -0,0 +1,20 @@
+var express = require('express');
+
+var playerRouter = express.Router();
+
+var Player = require('../client/src/models/playerInventory.js');
+
+var DbQueryHelper = require('../db/dbQueryHelper.js');
+var query = new DbQueryHelper();
+
+//player inventory index
+playerRouter.get('/', function(req,res){
+  query.allPlayerItems(function(playerItems){
+    res.json(playerItems)
+  })
+});
+
+
+
+
+module.exports = playerRouter;
