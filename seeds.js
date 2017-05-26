9c2c54741ac7664b2dd7181e0e9a2bdc5789c1c5
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-05-26 11:15:04 +0100

    seeds set up

---
+++ seeds.js
@@ -0,0 +1,31 @@
+use codeclan_bar;
+
+db.player_inventory.insert([
+  {
+    name: "Amstel",
+    value: 4
+  },
+]);
+
+db.bar_inventory.insert([
+  {
+    name: "Amstel",
+    value: 4
+  },
+  {
+    name: "Peroni",
+    value: 6
+  },
+  {
+    name: "apple juice",
+    value: 2
+  },
+  {
+    name: "Sex on the beach",
+    value: 9
+  },
+  {
+    name: "Guinness",
+    value: 3
+  },
+]);
