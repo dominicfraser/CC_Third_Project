22f651f6c6f7031afc060a1fce8b21faef138cb8
David Robertson <davidjrobertson@live.co.uk>
2017-05-27 12:44:17 +0100

    made specs folder and file, all tests passed

--- client/src/models/specs/specs.js
+++ client/src/models/specs/specs.js
@@ -10,11 +10,32 @@ describe('Specs', function() {
   var player
 
   beforeEach(function() {
-    bar = new Bar("Chanter", 1000);
-    item = new Item()
+    bar = new Bar({name: "Chanter", cashDrawer: 1000});
+    item = new Item({name: "Amstel", value: 5});
+    player = new Player({name: "David", wallet: 100});
   });
 
-  it('should have title titanic', function() {
-    assert.equal(film.title, 'Titanic');
+  it('bar should have name of Chanter', function() {
+    assert.equal(bar.name, 'Chanter');
+  });
+
+  it('bar should have 1000 in cash drawer', function() {
+    assert.equal(bar.cashDrawer, 1000);
+  });
+
+  it('item should have name of Amstel', function() {
+    assert.equal(item.name, 'Amstel');
+  });
+
+  it('item should have value of 5', function() {
+    assert.equal(item.value, 5);
+  });
+
+  it('player should have name of David', function() {
+    assert.equal(player.name, "David");
+  });
+
+  it('player should have wallet value of 100', function() {
+    assert.equal(player.wallet, 100);
   });
 });
