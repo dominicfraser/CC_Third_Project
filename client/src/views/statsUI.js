b5ce87096ec12187e8618fa84abb17cc215f1a2b
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-05-28 18:12:03 +0100

    remaining moneyChart files

--- client/src/views/statsUI.js
+++ client/src/views/statsUI.js
@@ -1,14 +1,14 @@
-//graphs and stuff
-
-var ModelsContainer = require('../models/models_container')
+var ModelsContainer = require('../models/models_container');
+var MoneyChart = require('../../build/public/moneyChart.js');
 
 var StatsUI = function(player, bar){
   this.player = player;
   this.bar = bar;
 
+  console.log(this.player.wallet);
   var modelsContainer = new ModelsContainer;
-
-  
+    var moneyChart = new MoneyChart(this.player.wallet);
+    
 
   };
 
