959df4f47460681619d7b6be3033e2f81753e93a
Dominic <mrdjfraser@gmail.com>
2017-05-29 23:00:45 +0100

    refactoring inventoryUI and adding refresh blats for tables

--- client/src/views/inventoryUI.js
+++ client/src/views/inventoryUI.js
@@ -7,20 +7,19 @@ var InventoryUI = function(player, bar){
   var modelsContainer = new ModelsContainer;
 
   modelsContainer.allPlayerItems(function(playerItems){
-    this.renderPlayerItemsCount(playerItems);
+    this.renderPlayerItemsImages(playerItems);
     this.renderPlayerItemsCountDropdown(playerItems);
   }.bind(this));
   
 
   modelsContainer.allBarItems(function(barItems){
-    this.renderBarItemsCount(barItems);
     this.renderBarItemsCountDropdown(barItems);
-  }.bind(this));
+    this.renderBarItemsImages(barItems);
+  }.bind(this)); 
 
 };
 
 InventoryUI.prototype = {
-
   renderPlayerItemsCountDropdown: function(playerItems){
         var select = document.getElementById("player-inventory-dropdown");
         select.innerHTML = "";
@@ -36,10 +35,9 @@ InventoryUI.prototype = {
           select.appendChild(option);
         }
       },
-
-  renderPlayerItemsCount: function(playerItems){
-    
+  renderPlayerItemsImages: function(playerItems){
    var tablePicture = document.getElementById("player-inventory-picture")
+   tablePicture.innerHTML = ""
 
     var playerItemsWithCount = this.addCounts(playerItems)
     var playerItemsFiltered = this.filterToUniqList(playerItemsWithCount)
@@ -86,65 +84,40 @@ InventoryUI.prototype = {
         var imageCocktailButton = document.createElement('button')
         imageCocktailButton.innerHTML = "<img src =http://az659704.vo.msecnd.net/v1/image/c_lpad,w_1500,h_1500/v1400600701/cocktail_manhattan-1.png>";
 
-          td.appendChild(imageCocktailButton);
-          tablePicture.appendChild(td);
+        td.appendChild(imageCocktailButton);
+        tablePicture.appendChild(td);
       }
     }
   },
 
-  renderBarItemsCount: function(barItems){
-
+  renderBarItemsImages: function(barItems){
    var tablePicture = document.getElementById("bar-inventory-picture")
-
-    var barItems = this.getAllNames(barItems);
+   tablePicture.innerHTML = ""
+    var barItems = this.addCounts(barItems);
     var barItemsFiltered = this.filterToUniqList(barItems);
 
     for (var item of barItemsFiltered){
-      if (item.name === "Peroni"){
       var td = document.createElement('td')
-      var imagePeroniButton = document.createElement('button')
-      imagePeroniButton.innerHTML = "<img src = https://images-na.ssl-images-amazon.com/images/I/71wvhFzpnrL._SX385_.jpg>"
+      var imageDrinkButton = document.createElement('button')
+      imageDrinkButton.innerHTML = "<img src = http://icons.iconarchive.com/icons/iconshock/brilliant-food/256/beer-icon.png>"
 
-        td.appendChild(imagePeroniButton);
-        tablePicture.appendChild(td);
-
-      }
-      else if (item.name === "Amstel"){
-      var td = document.createElement('td')
-      var imageAmstelButton = document.createElement('button')
-      imageAmstelButton.innerHTML = "<img src = http://www.beersofeurope.co.uk/media/catalog/product/cache/1/image/298x308/9df78eab33525d08d6e5fb8d27136e95/pimages/Amstel330ml.jpg>";
-
-        td.appendChild(imageAmstelButton);
-        tablePicture.appendChild(td);
-
-        } 
-        else if (item.name === "Guinness"){
-        var td = document.createElement('td')
-        var imageGuinnessButton = document.createElement('button')
-        imageGuinnessButton.innerHTML = "<img src = http://3.bp.blogspot.com/-DOthabqw9gQ/UtG_OpxcyFI/AAAAAAAAAEQ/yVjnr0-nO58/s1600/Guinness+single+glass.jpg>";
-
-          td.appendChild(imageGuinnessButton);
-          tablePicture.appendChild(td);
-
-        }  
-        else if (item.name === "Apple Juice"){
-        var td = document.createElement('td')
-        var imageAppleJuiceButton = document.createElement('button')
-        imageAppleJuiceButton.innerHTML = "<img src = http://www.foodsafetynews.com/files/2013/07/applejuice_406.jpg>";
-
-          td.appendChild(imageAppleJuiceButton);
-          tablePicture.appendChild(td);
-
-          } 
-        else if (item.name === "Cocktail"){
-        var td = document.createElement('td')
-        var imageCocktailButton = document.createElement('button')
-        imageCocktailButton.innerHTML = "<img src =http://az659704.vo.msecnd.net/v1/image/c_lpad,w_1500,h_1500/v1400600701/cocktail_manhattan-1.png>";
-
-          td.appendChild(imageCocktailButton);
-          tablePicture.appendChild(td);
+      td.appendChild(imageDrinkButton);
+      tablePicture.appendChild(td);
     }
-  } 
+  },
+  renderBarItemsCountDropdown: function(barItems){
+    var select = document.getElementById("bar-inventory-dropdown");
+    select.innerHTML = "";
+
+    var barItemsWithCount = this.addCounts(barItems)
+    var barItemsFiltered = this.filterToUniqList(barItemsWithCount)
+
+    for (var item of barItemsFiltered){
+      var option = document.createElement("option");
+      option.innerText = item.name + ' (' + item.count + ')';
+      option.value = item.id;
+      select.appendChild(option);
+    }
   },
 
   addCounts: function (items) {
@@ -172,21 +145,6 @@ InventoryUI.prototype = {
       return itemNames.indexOf(item.name) == index
     })      
     return filtered
-  },
-
-  renderBarItemsCountDropdown: function(barItems){
-    var select = document.getElementById("bar-inventory-dropdown");
-    select.innerHTML = "";
-
-    var barItemsWithCount = this.addCounts(barItems)
-    var barItemsFiltered = this.filterToUniqList(barItemsWithCount)
-
-    for (var item of barItemsFiltered){
-      var option = document.createElement("option");
-      option.innerText = item.name + ' (' + item.count + ')';
-      option.value = item.id;
-      select.appendChild(option);
-    }
   },
 };
 
