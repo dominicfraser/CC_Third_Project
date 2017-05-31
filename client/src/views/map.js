bdbaedf9a4e4be84afeafb3a323b50856bc73879
David Robertson <davidjrobertson@live.co.uk>
2017-05-31 11:30:22 +0100

    person can now only be interacted with when in the coordinates surrounding him

--- client/src/views/map.js
+++ client/src/views/map.js
@@ -93,8 +93,13 @@ Map.prototype = {
 
 
   interactWithMan: function(e){
+    var positionX = this.currentPosition[0];
+    var positionY = this.currentPosition[1];
     if(e.key === "p"){
-      this.interactionUI.speakToMan();
+      if((positionX >= 400 && positionX <= 490) && (positionY >= 170 && positionY <= 290)) 
+      {
+        this.interactionUI.speakToMan();
+      } else {return;}
     }
   },
 
